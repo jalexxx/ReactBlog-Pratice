9067fb2a20674160c29c266be8ffb766e5619876
jalexxx <jumaanahalex@gmail.com>
2022-02-27 10:04:47 +0000

    the root component, navbar component as well as home component

---
+++ react-blog/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Welcome to the new blog' 
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="content">
+        <Home></Home>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
